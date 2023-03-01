01652b83c7022f221503f23250bbb0b2af3f3308
Jamar <69270601+harrisj1j@users.noreply.github.com>
2023-02-28 19:00:33 -0500

    Add files via upload

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,22 +1,13 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "./firebase";
 import React, {useState} from "react";
-import {Forgotpass} from './Forgotpass';
-import {Link} from "react-router-dom"
-import menuLogo from './img/JAMS_1563X1563.png'
-import { useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
+
+
 
 export const Login = (props) =>{
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
 
-    {/*start app on login screen */}
-  const [currentForm, setCurrentForm] = useState('login');
-  const toggleForm = (formName) =>{
-    setCurrentForm(formName);
-  }
-  
     {/* event handler for form entry*/}
     
     const logIn = (e) => {
@@ -24,41 +15,28 @@ export const Login = (props) =>{
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             console.log(userCredential)
-            navigate("/adminhome");
-
         }).catch((error) => {
             console.log(error);
         })
-        
-    }
 
+    }
     //login form
     return(
-        
-        <><div className = "big-logo">
-            <img src={menuLogo} alt="logo"/>
-
-        </div>
         <div className="auth-form-container">
             <form className="login-form" onSubmit={logIn}>
                 <h2>Login</h2>
                 <label htmlFor="email">Username</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="username (your email)" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="username (your email)" id="email" name="email"/>
                 <label htmlFor="password">password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="******" id="password" name="password" />
-                <button type="submit" >Log In</button>    
+                <input value={password} onChange={(e) =>setPassword(e.target.value) } type="password" placeholder="******" id="password" name="password"/>
+                <button type="submit">Log In</button>
             </form>
             {/* switch to password form*/}
-            <Link to="/forgotpass">
-                <button className="link-btn">Forgot password</button>
-            </Link>
-            <Link to="/register">
-                <button className="link-btn">Register New User</button>
-            </Link>
+            <button className="link-btn" onClick={() =>props.onFormSwitch('forgotpass')}>Forgot password</button>
 
-
-        </div></>
-        
+            
+            
+        </div>
     );
 }
 
